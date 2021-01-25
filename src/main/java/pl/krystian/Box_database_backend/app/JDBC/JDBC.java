b7692931f137efac8ae1646ccb53e87962a49092
433MHz <krystian.izdebski1999@gmail.com>
2021-01-25 19:28:26 +0100

    Box count method added in JDBC
    This method is used to get amount of boxes saved in database

--- src/main/java/pl/krystian/Box_database_backend/app/JDBC/JDBC.java
+++ src/main/java/pl/krystian/Box_database_backend/app/JDBC/JDBC.java
@@ -3,6 +3,7 @@ package pl.krystian.Box_database_backend.app.JDBC;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import org.springframework.stereotype.Component;
@@ -12,42 +13,62 @@ public class JDBC {
 	private Connection conn;
 	private Statement stmt;
 	private ResultSet rs;
-	
+
 	private final String driver = "com.mysql.cj.jdbc.Driver";
 	private final String url = "jdbc:mysql://localhost/pudelka";
-	
+
 	private final String user = "root";
 	private final String pass = "";
-	
-	public ResultSet use(String query) {
+
+	private void Connect() {
 		try {
 			Class.forName(driver);
 			conn = DriverManager.getConnection(url, user, pass);
 			stmt = conn.createStatement();
+		} catch (Exception e) {
+			Disconnect();
+		}
+	}
+
+	private void Disconnect() {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+		} catch (Exception e) {
+			System.out.println("Error while closing ResultSet" + e);
+		}
+
+		try {
+			if (stmt != null) {
+				stmt.close();
+			}
+		} catch (Exception e) {
+			System.out.println("Error while closing Statement" + e);
+		}
+
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			System.out.println("Error while closing Connection" + e);
+		}
+
+	}
+
+	public int CountBoxes() {
+		Connect();
+		String query = "SELECT COUNT(id) AS count FROM boxes";
+		int count = 0;
+		try {
 			rs = stmt.executeQuery(query);
 			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if(stmt!=null) {
-					stmt.close();
-				}
+			while(rs.next()) {
+				count = rs.getInt("count");
 			}
-			catch (Exception e) {
-				System.out.println("Error while closing Statement" + e);
-			}
-			
-			try {
-				if(conn!=null) {
-					conn.close();
-				}
-			}
-			catch (Exception e) {
-				System.out.println("Error while closing connection" + e);
-			}
-		}
-		return rs;
+		} catch (SQLException e) {e.printStackTrace();}
+		finally {Disconnect();}
+		return count;
 	}
 }
